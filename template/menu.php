e0fe152ef0753f71c0cbb304359da551add2ef5b
tunarider <tunarider@tunaground.net>
2020-08-01 18:03:59 +0900

    trace 게시판 링크 추가

--- template/menu.php
+++ template/menu.php
@@ -15,6 +15,7 @@ $traceList = '';
 if ($_SERVER['SCRIPT_NAME'] === "{$baseUrl}/trace.php") {
     $maxResponseView = $board['maxResponseView'];
     $traceList = <<<HTML
+<li><a href="$baseUrl/index.php/{$board['uid']}">게시판으로</a></li>
 <li><a href="$baseUrl/trace.php/{$board['uid']}/$threadUid/recent">최근 $maxResponseView 보기</a></li>
 <li><a href="$baseUrl/trace.php/{$board['uid']}/$threadUid">전부 보기</a></li>
 HTML;
