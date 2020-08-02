41608f90366c2e3de4f5cb08ee83ad504d44dcf1
tunarider <tunarider@tunaground.net>
2020-08-02 10:22:38 +0900

    remove undefined variable board

--- error.php
+++ error.php
@@ -9,7 +9,7 @@ $config = require('./require.php');
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <meta name="format-detection" content="telephone=no">
-    <title>오류 :: <?= $board['name'] ?></title>
+    <title>오류</title>
 </head>
 <body>
 <p>
